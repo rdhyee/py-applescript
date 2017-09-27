6b0de0f3965fedd2f29370deb14a3cf456d33150
Raymond Yee <raymond.yee@gmail.com>
2017-09-26 21:13:40 -0700

    source from https://pypi.python.org/packages/83/a0/d4f8cbae9a0e0fc0219fa44e3c2ed3b88caa5988e83c6118c733e9447e0a/py-applescript-1.0.0.tar.gz#md5=8f601fabe2e3d143e637e86fe57ffa3c

---
+++ setup.py
@@ -0,0 +1,22 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+		name = "py-applescript",
+		version = "1.0.0",
+		description = "An easy-to-use Python wrapper for NSAppleScript, allowing Python scripts to communicate with AppleScripts and AppleScriptable applications.",
+		platforms = ['Mac OS X'],
+		license = 'Public Domain',
+		packages = ['applescript'],
+		requires = ['pyobjc'],
+		classifiers = [
+			'License :: Public Domain',
+			'Development Status :: 5 - Production/Stable',
+			'Operating System :: MacOS :: MacOS X',
+			'Programming Language :: Python :: 2.7',
+			'Programming Language :: Python :: 3',
+		],
+
+)
