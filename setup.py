ce18d54ccca0614cd39fa73cd44dbceb0d5045a5
Raymond Yee <raymond.yee@gmail.com>
2022-01-22 20:34:06 -0800

    fixes #012 -- thanks to @necklinux for reporting the issue and showing the fix

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ except ImportError:
 
 setup(
 		name = "py-applescript",
-		version = "1.0.2",
+		version = "1.0.3",
 		description = "An easy-to-use Python wrapper for NSAppleScript, allowing Python scripts to communicate with AppleScripts and AppleScriptable applications.",
 		url = "https://github.com/rdhyee/py-applescript",
 		platforms = ['Mac OS X'],
 		license = 'Public Domain',
 		packages = ['applescript'],
-		requires = ['pyobjc-core', 'pyobjc-framework-AppleScriptKit', 'pyobjc-framework-AppleScriptObjC'],
+		install_requires = ['pyobjc-core', 'pyobjc-framework-AppleScriptKit', 'pyobjc-framework-AppleScriptObjC'],
 		classifiers = [
 			'License :: Public Domain',
 			'Development Status :: 5 - Production/Stable',
